5afd69d3c549b4878d7f5b035c8d69d6b811b843
Vorobev Vsevolod <100051617+vorobevvsevolod@users.noreply.github.com>
2023-08-28 17:55:03 +0300

    ffff

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
 $(function() {
-    let header = $("#header"),
-        introH = $("#about").innerHeight(),
-        scrollOffset = $(window).scrollTop();
-
-    introH *=2;
-        // Header 
-    
-
-    $(window).on("scroll", function () {
-        scrollOffset = $(this).scrollTop();
-        
-        checkScroll(scrollOffset);
-
-    });
-
-    function checkScroll(scrollOffset){
-        
-        if(scrollOffset >= introH) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed")
-        }      
-        
-        
-    }
-
     // smothScroll 
 
     $("[data-scroll]").on("click", function(event){
